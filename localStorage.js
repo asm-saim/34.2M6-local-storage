82d1e1b1ce79432a0c64743ef6e909ad0ac6cf7b
asm-saim <saim777m@gmail.com>
2026-06-25 00:10:42 +0600

    set and get item of an obj

--- localStorage.js
+++ localStorage.js
@@ -8,4 +8,21 @@ const addRandomNumbers = () => {
 const showRandomNumbers = () => {
     const showValue = localStorage.getItem("Number")
     console.log("Random Numbers added:", showValue)
-} 
+}
+
+//assign non-primitive value: object
+
+//SET OBJECT:
+const setObjectToLS = () => {
+    const friends = { name: "Abdul karim", age: 45, location: "Pabna" };
+    const ObjectJSON = JSON.stringify(friends)
+    const setObj = localStorage.setItem("friends", ObjectJSON);
+    console.log(setObj)
+}
+
+// GET OBJECT
+const getObj = () => {
+    const getTheObject = localStorage.getItem("friends");
+    const ObjConvert = JSON.parse(getTheObject);
+    console.log(ObjConvert);
+}
